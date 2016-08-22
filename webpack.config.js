e9eb80e25cbc84f475c068e64df0b125a5b13c13
hnlichong <543281892@qq.com>
2016-08-22 10:08:04 +0800

    update  entry: Object, output corresponding .js

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,30 @@
 var webpack = require('webpack');
+var fs = require("fs");
 
+var getDirFilesPathArr = function(path){
+  var dirFilesPathArr = {};
+  var fileName = '';
+  var fileArr = fs.readdirSync(path);
+  fileArr.forEach( function (file,index){
+    fileName =  file.match(/(.+)\.js$/)[1];
+    dirFilesPathArr[fileName] = path + file;
+  });
+  return dirFilesPathArr;
+};
 module.exports = {
   context: __dirname,
-  entry: './js/index.js',
+  entry: getDirFilesPathArr(__dirname + '/src/js/'),
   output: {
-    path: __dirname + '/build',
-    publicPath: '/build/',
-    filename: 'index.js'
+    path: __dirname + '/build/',
+    publicPath: 'build/',
+    filename: '[name].js'
   },
   module: {
     //preloaders:[
     //  { test: /\.js$/, loader: 'eslint', include: /js/}
     //],
     loaders: [
-      { test: /\.js$/, loader: 'babel', include: /js/, query:{presets:['es2015','react']}}
+      { test: /\.js$/, loader: 'babel', include: /js/, exclude: /node_modules/, query:{presets:['es2015','react']}}
       //{ test: /\.css$/,  loader: "style!css" },
       //{ test: /\.jp(e)?g|png|gif|svg|ttf|eot|woff|woff2$/, loader:'url?limit=8192&name=./build/[hash].[ext]'},
       //{ test: /\.svg|ttf|eot|woff|woff2$/, loader:'file&name=./build/[hash].[ext]'}
